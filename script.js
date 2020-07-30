0792b27fa61b60634a5dabb503f2d1b3289ac611
Stephen White <smw444@nau.edu>
2020-07-30 07:11:50 -0700

    Basic Chat App with 1 Room Complete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const socket = io('http://localhost:3000/');
 let messageContainer = document.getElementById('messages');
 let messageForm = document.getElementById('send-container');
 let messageInput = document.getElementById('message-input');
+let someoneTypingText = document.getElementById('someone-typing');
 
 const getTimeStamp = () => {
     return new Date().toLocaleString([], {
@@ -43,8 +44,13 @@ const appendSentMessage = message => {
 }
 
 const name = prompt('What is your name?');
-socket.emit('new-join', {
+socket.emit('user-connected', {
     message: `${name} Joined the Chat`,
+    user: name
+})
+
+socket.on('user-refresh', () => {
+    socket.emit('refresh-user', name);
 })
 
 socket.on('sent-message', data => {
@@ -55,10 +61,61 @@ socket.on('recieved-chat-message', data => {
     appendRecievedMessage(data.message, data.user);
 })
 
+socket.on('user-disconnected', name => {
+    appendRecievedMessage(`${name} disconnected`);
+})
+
+socket.on('notify-users-someone-is-typing', user => {
+    if(user) {
+        someoneTypingText.innerHTML = `${user} is typing`;
+    }
+})
+
+socket.on('clear-user-is-typing', () => {
+    someoneTypingText.innerHTML = '';
+})
+
+
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
-    socket.emit('send-chat-message', {message: message, user: name});
-    messageInput.value = '';
+    if(message) {
+        socket.emit('send-chat-message', { message: message, user: name });
+        socket.emit('user-is-done-typing');
+        messageInput.value = '';
+    }
 })
 
+const debounce = (fnc, wait, immediate) => {
+    let timeout;
+
+    return function executedFunction() {
+        let context = this;
+        let args = arguments;
+
+        let later = () => {
+            timeout = null;
+            if(!immediate) fnc.apply(context, args);
+        };
+
+        let callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(later, wait);
+
+        if(callNow) fnc.apply(context, args);
+    }
+}
+
+const notifyUserIsDoneTyping = debounce(() => {
+    socket.emit('user-is-done-typing');
+}, 1500)
+
+messageInput.addEventListener('keypress', e => {
+    if (e.key !== 'Enter') {
+        socket.emit('user-is-typing');
+    }
+})
+
+messageInput.addEventListener('keypress', notifyUserIsDoneTyping);
--- server.js
+++ server.js
@@ -5,24 +5,65 @@ const port = 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+io.eio.pingTimeout = 120000; // 2 minutes
+io.eio.pingInterval = 5000;  // 5 seconds
+
+const users = {};
+
+const updateAllClients = (socket, data) => {
+    if(!users[socket.id]) {
+        socket.emit('user-refresh');
+    } 
+    socket.broadcast.emit('recieved-chat-message', { message: data.message, user: users[socket.id] });
+    socket.emit('sent-message', { message: data.message });
+    
+}
+
+const handleNewUserConnected = (socket, data) => {
+    console.log(`${data.user} Connected!`);
+    users[socket.id] = data.user;
+    socket.broadcast.emit('recieved-chat-message', { message: data.message });
+    socket.emit('sent-message', { message: data.message, user: undefined });
+}
+
+const handleUserDisconnected = (socket) => {
+    if(users[socket.id]) {
+        console.log(`${users[socket.id]} Disconnected!`);
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    }
+}
 
 io.on('connection', socket => {
-    console.log('New Connection');
 
-    socket.on('new-join', data => {
-        socket.broadcast.emit('recieved-chat-message', { message: data.message});
-        socket.emit('sent-message', { message: data.message, user:undefined });
+    socket.on('user-connected', data => {
+        handleNewUserConnected(socket, data);
     });
+
+    socket.on('refresh-user', name => {
+        users[socket.id] = name;
+    })
 
     socket.on('send-chat-message', data => {
-        socket.broadcast.emit('recieved-chat-message', {message: data.message, user: data.user});
-        socket.emit('sent-message',{message: data.message});
+        updateAllClients(socket, data);
     });
-});
 
-io.on('disconnect', () => {
-    console.log('Client Disconnected!');
-})
+    socket.on('user-is-typing', () => {
+        if(!users[socket.id]) {
+            socket.emit('user-refresh');
+        }
+        socket.broadcast.emit('notify-users-someone-is-typing', 
+        users[socket.id]);
+    })
+
+    socket.on('user-is-done-typing', () => {
+        socket.broadcast.emit('clear-user-is-typing');
+    })
+
+    socket.on('disconnect', () => {
+        handleUserDisconnected(socket);
+    })
+});
 
 server.listen(port, () => console.log(`\x1b[40m`, `\x1b[32m`,
     `
